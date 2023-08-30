f2c697c4cc0587d5b7161899e7f5fd893ca1ce82
sushanthtambe <sushanth.tambe@gmail.com>
2023-08-30 16:30:04 +0530

    nav bar learning git commmit  push and pull

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div>
       <h1>App</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ FE/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Sushanth Navbar</div>
+  )
+}
+
+export default Navbar
