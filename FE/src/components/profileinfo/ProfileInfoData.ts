06a3e9e60d4c14fda4d9e2994bb1ecc55690012d
sushanthtambe <sushanth.tambe@gmail.com>
2023-09-03 19:46:20 +0530

    built profile Info component

---
+++ FE/src/components/profileinfo/ProfileInfoData.ts
@@ -0,0 +1,13 @@
+export const profileinfodetail=[
+    {
+        id:0,
+        userImage:'ImageURL',
+        userName:'Name',
+        desc:'Short Description',
+        rating:{
+            bltiz:'1000',
+            bullet:'1000',
+            rapid:'1000'
+        }
+    }
+]
---
+++ FE/src/components/profileinfo/ProfileInfoTypes.ts
@@ -0,0 +1,13 @@
+
+export interface profileinfotypes{
+    id:number,
+    UserImage:string,
+    Username:string,
+    Desc:string,
+    Rating:[
+        Bltiz:number,
+        bullet:number,
+        rapid:number,
+    ]
+
+}
